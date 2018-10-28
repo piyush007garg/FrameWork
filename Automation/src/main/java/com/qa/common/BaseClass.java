aaf10903f3117070ce9767e10a7d0e420272de50
unknown <piyush.garg@del1-lhp-N02511.synapse.com>
2018-10-28 22:31:41 +0530

    adding base class and some utile file

--- Automation/src/main/java/com/qa/common/BaseClass.java
+++ Automation/src/main/java/com/qa/common/BaseClass.java
@@ -1,5 +1,19 @@
 package com.qa.common;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+
 public class BaseClass {
+	public static WebDriver driver;
+
+	public BaseClass() throws IOException {
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(Constant.PAGE_LOAD_TIME_OUT, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Constant.IMPLICITY_TIME_OUT, TimeUnit.SECONDS);
+		driver.get(FileUtils.readProperties(Constant.CONFIG_PROPERTIES).getProperty("BaseURL"));
+	}
 
 }
---
+++ Automation/src/main/java/com/qa/common/Constant.java
@@ -0,0 +1,15 @@
+package com.qa.common;
+
+public class Constant {
+	
+	public static final long IMPLICITY_TIME_OUT= 20;
+	public static final long PAGE_LOAD_TIME_OUT= 20;
+	public static final String CONFIG_PROPERTIES= "Automation\\src\\main\\java\\com\\qa\\config\\config.properties";
+	
+	
+	private Constant(){
+		
+		
+	}
+
+}
