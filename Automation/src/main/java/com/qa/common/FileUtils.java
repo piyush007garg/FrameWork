969f18f520aae4386418a0650d6659ec584cd2e6
unknown <piyush.garg@del1-lhp-N02511.synapse.com>
2018-10-30 21:47:23 +0530

    add learning folder inside a project

--- Automation/src/main/java/com/qa/common/FileUtils.java
+++ Automation/src/main/java/com/qa/common/FileUtils.java
@@ -1,5 +1,29 @@
 package com.qa.common;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class FileUtils {
+	
+	public static void isFileExist(File file) throws FileNotFoundException {
+		if(!file.exists())
+			throw new FileNotFoundException("File not found in "+file.getAbsolutePath());
+	}
+		
+	
+	public static Properties readProperties(String filePath) throws IOException {
+		File file = new File(filePath + ".properties");
+		isFileExist(file);
+		Properties properties = new Properties();
+		InputStream in = new FileInputStream(file);
+		properties.load(in);
+		in.close();
+		return properties;
+		
+	}
 
 }
---
+++ Automation/src/main/java/com/qa/learning/SomeImportantPrograms.java
@@ -0,0 +1,67 @@
+package com.qa.learning;
+
+public class SomeImportantPrograms {
+
+	/**
+	 * fibonicii series
+	 */
+
+	// public static void main(String[] args) {
+	//
+	// int n=8;
+	// int i = 0,temp;
+	// int n1=0,n2=1,n3;
+	// System.out.println(n1+n2);
+	//
+	// for(i =0;i<n;i++) {
+	// n3 = n1+n2;
+	// System.out.println(n3);
+	// n1=n2;
+	// n2=n3;
+	//
+	//
+	// }
+	// }
+
+	/**
+	 * 
+	 * palindrome of integer number
+	 */
+//	public static void main(String[] args) {
+//
+//		int number = 123;
+//		int sum = 0;
+//		int temp;
+//		temp = number;
+//
+//		while (number > 0) {
+//			int reminder = number % 10;
+//			sum = (sum * 10) + reminder;
+//			number = number / 10;
+//		}
+//
+//		if (sum == temp) {
+//			System.out.println("paimdrome");
+//		} else {
+//			System.out.println("Nit palindrome");
+//		}
+//
+//	}
+	
+	
+	/**
+	 * Swap a number without third variable
+	 */
+	
+	 public static void main(String[] args) {
+		 int a=12 , b=23;
+		 
+		  a= a+b; // a=35
+		  b= a-b; //35-23 b = 12
+		  a=a-b;  //35-12 a = 23
+		  
+		  System.out.println("after swaping a,b the values of a is-->"+a +"and valueof b i--->"+b);
+		
+	}
+	
+}
